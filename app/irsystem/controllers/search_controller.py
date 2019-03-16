5ea789ba9799f094883aa814b85d417535ac1011
Greg Schultz <gjs86@cornell.edu>
2019-03-16 15:24:32 -0400

    netIDS for fork test

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -8,13 +8,20 @@ net_id = "Ilan Filonenko: if56"
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
+	netIDS = [
+		"gjs86",
+		"tt397",
+		"ng352",
+		"xl379",
+		"iyw3"
+	]
 	if not query:
 		data = []
 		output_message = ''
 	else:
 		output_message = "Your search: " + query
 		data = range(5)
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data, netIDS = netIDS)
 
 
 
