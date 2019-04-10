5733233145abec20372aaae01e2eaa9ea86a008f
Greg Schultz <gjs86@cornell.edu>
2019-04-09 21:36:48 -0400

    plumbing for splash page

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,8 +2,8 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "TEAM SOLEMATE"
+net_id = "dnm"
 
 @irsystem.route('/', methods=['GET'])
 def search():
@@ -22,6 +22,11 @@ def search():
 		output_message = "Your search: " + query
 		data = range(5)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data, netIDS = netIDS)
+
+
+@irsystem.route('/splash', methods=['GET'])
+def splash():
+	return render_template('splash.html')
 
 
 
