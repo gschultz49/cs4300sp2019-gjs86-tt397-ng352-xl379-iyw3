d8ddeb058a58bd8800ac37bbe51d5b8c3461f891
Greg Schultz <gjs86@cornell.edu>
2019-04-10 15:57:57 -0400

    favicon

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -28,10 +28,22 @@ def search():
 def splash():
 	query = request.args.get('search')
 	print ("USERS QUERY: {0}".format(query))
-	# return array of dictionary of shoes with information retrieval here run information retrieval here, make sure template names match up
+	# return array of dictionary of shoes with information retrieval here make sure template names match up
 
-	data = [ { "shoeName": "Nike Air Zoom Pegasus 35", "shoeImage": "https://cdn.runrepeat.com/i/nike/28162/nike-womens-air-zoom-pegasus-35-running-shoes-8-5-b-us-elemental-rose-barely-rose-vintage-wine-womens-elemental-rose-barely-rose-5627-main.jpg", "shoeURL": "https://runrepeat.com/nike-air-zoom-pegasus-35" }, { "shoeName": "Nike Air Zoom Pegasus 35", "shoeImage": "https://cdn.runrepeat.com/i/nike/28162/nike-womens-air-zoom-pegasus-35-running-shoes-8-5-b-us-elemental-rose-barely-rose-vintage-wine-womens-elemental-rose-barely-rose-5627-main.jpg", "shoeURL": "https://runrepeat.com/nike-air-zoom-pegasus-35" }, { "shoeName": "Nike Air Zoom Pegasus 35", "shoeImage": "https://cdn.runrepeat.com/i/nike/28162/nike-womens-air-zoom-pegasus-35-running-shoes-8-5-b-us-elemental-rose-barely-rose-vintage-wine-womens-elemental-rose-barely-rose-5627-main.jpg", "shoeURL": "https://runrepeat.com/nike-air-zoom-pegasus-35" }, { "shoeName": "Nike Air Zoom Pegasus 35", "shoeImage": "https://cdn.runrepeat.com/i/nike/28162/nike-womens-air-zoom-pegasus-35-running-shoes-8-5-b-us-elemental-rose-barely-rose-vintage-wine-womens-elemental-rose-barely-rose-5627-main.jpg", "shoeURL": "https://runrepeat.com/nike-air-zoom-pegasus-35" }, { "shoeName": "Nike Air Zoom Pegasus 35", "shoeImage": "https://cdn.runrepeat.com/i/nike/28162/nike-womens-air-zoom-pegasus-35-running-shoes-8-5-b-us-elemental-rose-barely-rose-vintage-wine-womens-elemental-rose-barely-rose-5627-main.jpg", "shoeURL": "https://runrepeat.com/nike-air-zoom-pegasus-35" } ]
+	results = [ 
+		{ 
+			"shoeName": "Nike Air Zoom Pegasus 35", 
+			"shoeImage": "https://cdn.runrepeat.com/i/nike/28162/nike-womens-air-zoom-pegasus-35-running-shoes-8-5-b-us-elemental-rose-barely-rose-vintage-wine-womens-elemental-rose-barely-rose-5627-main.jpg", 
+			"shoeURL": "https://runrepeat.com/nike-air-zoom-pegasus-35" 
+			}, 
+		{ 
+			"shoeName": "Nike Epic React Flyknit", 
+			"shoeImage": "https://cdn.runrepeat.com/i/nike/27379/nike-men-s-epic-react-flyknit-running-shoes-11-5-grey-white-mens-grey-white-aad2-main.jpg",
+			"shoeURL": "https://runrepeat.com/nike-epic-react-flyknit"  
+		}, 
+	# random filler for grid testing
+	{ "shoeName": "Nike Air Zoom Pegasus 35", "shoeImage": "https://cdn.runrepeat.com/i/nike/28162/nike-womens-air-zoom-pegasus-35-running-shoes-8-5-b-us-elemental-rose-barely-rose-vintage-wine-womens-elemental-rose-barely-rose-5627-main.jpg", "shoeURL": "https://runrepeat.com/nike-air-zoom-pegasus-35" }, { "shoeName": "Nike Air Zoom Pegasus 35", "shoeImage": "https://cdn.runrepeat.com/i/nike/28162/nike-womens-air-zoom-pegasus-35-running-shoes-8-5-b-us-elemental-rose-barely-rose-vintage-wine-womens-elemental-rose-barely-rose-5627-main.jpg", "shoeURL": "https://runrepeat.com/nike-air-zoom-pegasus-35" }, { "shoeName": "Nike Air Zoom Pegasus 35", "shoeImage": "https://cdn.runrepeat.com/i/nike/28162/nike-womens-air-zoom-pegasus-35-running-shoes-8-5-b-us-elemental-rose-barely-rose-vintage-wine-womens-elemental-rose-barely-rose-5627-main.jpg", "shoeURL": "https://runrepeat.com/nike-air-zoom-pegasus-35" } ]
 
-	return render_template('splash.html', data = data )
+	return render_template('splash.html', data = results )
 
 
