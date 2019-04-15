54b09bc70dc3878328cab033da3a36745a10a2d6
Greg Schultz <gjs86@cornell.edu>
2019-04-15 14:04:56 -0400

    simple card data

--- app/static/splash.js
+++ app/static/splash.js
@@ -1,34 +1,52 @@
 
 let $SCRIPT_ROOT = ""
-$(document).ready(function () {
+
+$(document).on("click", '.card', function (event) {
   // when user selects a specific card, grab its attributes to populate the modal
-  $(".card").click(function () {
-    let card = $(this)
-    // get contents of clicked shoe
-    let shoeName= card.find("h3.shoeName").text();
-    let shoeImage = card.find("img.shoeImage").attr("src");
+  let card = $(this);
+  console.log("CLICKED");
+  // get contents of clicked shoe
+  let shoeName = card.find(".card-shoeName").text();
+  let shoeImage = card.find(".card-shoeImage").attr("src")
+  let similarShoes = card.find(".card-similarShoes").text();
+  let corescore = card.find(".card-corescore").text();
+  let similarity = card.find(".card-similarity").text();
+  let relevantTerms = card.find(".card-relevantTerms").text();
 
-    console.log(shoeName, shoeImage);
 
-    // populate modal 
-    let modal = $(".modal-content");
-    modal.find(".modal-shoeName").text(shoeName);
-    modal.find(".modal-shoeImage").attr("href", shoeImage);
-    
-  });
+  console.log(shoeName, shoeImage, similarShoes, corescore, similarity, relevantTerms);
+
+  // populate modal 
+  let modal = $(".modal-content");
+  modal.find(".modal-shoeImage").attr("href", shoeImage);
+  modal.find(".modal-shoeName").text(shoeName);
+  modal.find(".modal-similarShoes").text(similarShoes);
+  modal.find(".modal-corescore").text(corescore);
+  modal.find(".modal-similarity").text(similarity);
+  modal.find(".modal-relevantTerms").text(relevantTerms);
+});
+
+$(document).ready(function () {
+  
 
   // rendering template for a card
   // will need to handle which mode we're in
   let render_card = (shoe) => {
-    console.log("Rendering template data");
+    // console.log("Rendering template data");
     let card_template =
     `
     <div class="card" data-toggle="modal" data-target="#exampleModalCenter">
       <figure>
-        <img class="shoeImage" src="${shoe.shoeImage}">
+        <img class="card-shoeImage" src="${shoe.shoeImage}">
       </figure>
       <div class="card-caption">
-        <h3 class="shoeName"> ${shoe.shoeName}</h3>
+        <h3 class="card-shoeName"> ${shoe.shoeName}</h3>
+      </div>
+      <div class="additional-data">
+        <p class="card-similarShoes"> ${shoe.similarShoes}</p>
+        <p class="card-corescore"> ${shoe.corescore}</p>
+        <p class="card-similarity"> ${shoe.similarity}</p>
+        <p class="card-relevantTerms"> ${shoe.relevantTerms}</p>
       </div>
     </div>
     `
