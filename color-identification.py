08af660ce5a0d0a0589d684802fdce604e6f9157
Greg Schultz <gjs86@cornell.edu>
2019-04-22 17:51:05 -0400

    color identification and arches pics

--- color-identification.py
+++ color-identification.py
@@ -1,16 +1,20 @@
-# https: // towardsdatascience.com/color-identification-in-images-machine-learning-application-b26e770c4c71
+# https://towardsdatascience.com/color-identification-in-images-machine-learning-application-b26e770c4c71
 
 import cv2
 import numpy as np
 from skimage import io
 import matplotlib.pyplot as plt
 import requests
+import pickle
 
+with open('save.p', 'rb') as pickle_file:
+    img = pickle.load(pickle_file)["shoe1"] [:, :, :-1]
 
 # img = io.imread('https://i.stack.imgur.com/DNM65.png')[:, :, :-1]
+# print (img)
 
-img = io.imread(
-    "https://cdn.runrepeat.com/i/nike/28162/nike-air-zoom-pegasus-35-mens-942851-002-black-oil-grey-white-6f1d-600.jpg")[:, :, :-1]
+# img = io.imread(
+#     "https://cdn.runrepeat.com/i/nike/28162/nike-air-zoom-pegasus-35-mens-942851-002-black-oil-grey-white-6f1d-600.jpg")[:, :, :-1]
 
 
 average = img.mean(axis=0).mean(axis=0)
@@ -38,6 +42,7 @@ dominant = palette[np.argmax(counts)]
 
 print (average)
 print (dominant)
+print ("rgb({0},{1},{2})".format(int(dominant[0]), int(dominant[1]), int(dominant[2])))
 
 # fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(12, 6))
 # ax0.imshow(avg_patch)
